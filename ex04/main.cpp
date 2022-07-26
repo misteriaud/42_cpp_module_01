186a68b79360d7be4c367671c93279466a7b035b
Maxime Riaud (at 42) <mriaud@student.42.fr>
2022-07-26 16:47:06 +0200

    fonctionne pas

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <fstream>
 
-std::string readFileIntoString(const std::string& path) {
-    std::ifstream input_file(path);
-    if (!input_file.is_open()) {
-        std::cerr << "Could not open the file - '"
-             << path << "'" << std::endl;
-			 return (NULL);
-    }
-	while (input_file.getline())
-    return std::string(input_file., input_file.end());
+std::string replace(const std::string& src, const std::string& from, const std::string& to)
+{
+	std::string		dest;
+	size_t			last_index = 0;
+	size_t			index;
+
+	while (index = src.find(from) != std::string::npos)
+	{
+		dest << src.substr(last_index, index);
+		dest << str2;
+		dest << tampon.substr(found + str1.length(), tampon.npos) << std::endl;
+	}
+	return (dest);
 }
 
 int main(int ac, char **av)
@@ -19,10 +23,20 @@ int main(int ac, char **av)
 		std::cerr << "Nombre d'arguments invalides\n";
 		return (1);
 	}
-	const std::string path = av[1];
-	const std::string str1 = av[2];
-	const std::string str2 = av[3];
+    std::ifstream		input_file(av[1]);
+	const std::string	str1 = av[2];
+	const std::string	str2 = av[3];
+	std::string			tampon;
 
+	if (!input_file.is_open())
+	{
+		std::cerr << "Could not open the file " << av[1] << std::endl;
+		return (1);
+	}
+	while (std::getline(input_file, tampon))
+	{
+		std::cout << replace(tampon, str1, str2) << std::endl;
+	}
 
 	return (0);
 }
