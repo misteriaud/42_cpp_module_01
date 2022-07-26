8b32cccddaf21fb38005b1199cfa1fa1d5f17491
Maxime Riaud <maxime.riaud@gmail.com>
2022-07-26 22:46:05 +0200

    Ex06 ok

---
+++ ex06/Harl.cpp
@@ -0,0 +1,70 @@
+#include "Harl.hpp"
+#include <iostream>
+#include "colors.h"
+
+Harl::Harl()
+{
+	_map[DEBUG].name = std::string(debug);
+	_map[DEBUG].func = &Harl::_debug;
+
+	_map[INFO].name = std::string(info);
+	_map[INFO].func = &Harl::_info;
+
+	_map[WARNING].name = std::string(warning);
+	_map[WARNING].func = &Harl::_warning;
+
+	_map[ERROR].name = std::string(error);
+	_map[ERROR].func = &Harl::_error;
+}
+
+Harl::~Harl()
+{
+}
+
+void	Harl::_debug(void)
+{
+	std::cout << "debug\n";
+}
+
+void	Harl::_info(void)
+{
+	std::cout << YELLOW << "info\n" << RESET;
+}
+
+void	Harl::_warning(void)
+{
+	std::cout << MAGENTA << "warning\n" << RESET;
+}
+
+void	Harl::_error(void)
+{
+	std::cout << RED << "error\n" << RESET;
+}
+
+void	Harl::complain(std::string level)
+{
+	size_t	i = 0;
+
+	while (i < 4)
+	{
+		if (_map[i].name == level)
+			break ;
+		i++;
+	}
+	switch (i)
+	{
+	case DEBUG:
+		_debug();
+		/* fall through */
+	case INFO:
+		_info();
+		/* fall through */
+	case WARNING:
+		_warning();
+		/* fall through */
+	case ERROR:
+		_error();
+		/* fall through */
+	}
+	std::cout << std::endl;
+}
---
+++ ex06/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl logger;
+	logger.complain(std::string("fwfwe"));
+	logger.complain(std::string(error));
+	logger.complain(std::string(warning));
+	logger.complain(std::string(info));
+	logger.complain(std::string(debug));
+	return (0);
+}
